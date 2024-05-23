25717bce47cde3b74cfa4915257d45380192f539
DanieleTrippodo <trippododaniele@gmail.com>
2024-05-23 15:00:49 +0200

    update content on JS

---
+++ Snack 1/script.js
@@ -0,0 +1,14 @@
+// Tavolo degli invitati
+const tableName = 'Tavolo Vip';
+
+
+
+// Lista delle persone invitate
+const guests = [
+    'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo',
+    'Georgina Rodriguez', 'Chiara Ferragni', 'George Clooney',
+    'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'
+];
+
+
+console.log('tutti gli invitati', guests);
